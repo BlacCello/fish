f644db237a0957644c806b3061b4d617f3527845
Marcel Henrich <marcel.henrich-extern@deutschebahn.com>
2022-07-16 21:35:29 +0200

    Use tide as prompt

--- README.md
+++ README.md
@@ -1,3 +1,22 @@
 # fish configuration
 
 For initial setup, execute the [create-symlinks.sh](./create-symlinks.sh) script. This will create symlink to the necessary directories in `~/.config`.
+
+Run `tide configure` after installation.
+
+## Dependencies
+
+### Nerd font
+
+MacOS installation:
+
+```shell
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
+```
+
+## fisher plugins
+
+```shell
+fisher install IlanCosman/tide@v5
+```
--- create-symlinks.sh
+++ create-symlinks.sh
@@ -5,13 +5,13 @@ set -eu -o pipefail
 script="$(readlink -f "$0")"
 dir="$(dirname $script)"
 
-folders=("config.fish" "conf.d")
+files=("config.fish" "conf.d/azure.fish" "conf.d/git.fish" "conf.d/k8s.fish")
 home_dir=~
 echo "Home directory is ${home_dir}"
 
-for folder in "${folders[@]}"; do
-  source="${dir}/${folder}"
-  target="${home_dir}/.config/fish/${folder}"
+for file in "${files[@]}"; do
+  source="${dir}/${file}"
+  target="${home_dir}/.config/fish/${file}"
 
   echo "Create symlink from ${source} to ${target}"
   rm -rf -- "$target"
