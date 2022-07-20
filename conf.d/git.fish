52997ebc391549690c1b8123ff18541b8023716e
Marcel Henrich <marcel.henrich-extern@deutschebahn.com>
2022-07-20 15:10:56 +0200

    Add repo function to find git repos

--- conf.d/git.fish
+++ conf.d/git.fish
@@ -1,10 +1,11 @@
 abbr -a g git
 abbr -a ga git add
+abbr -a gd git diff
 abbr -a gs git status
 abbr -a gb git switch
 abbr -a gm git switch main
 abbr -a gf git fetch --prune
-abbr -a gc git checkout
+abbr -a gc git commit
 abbr -a gu git pull
 abbr -a gp git push
 
@@ -15,3 +16,19 @@ end
 function pr
   git diff (git symbolic-ref refs/remotes/origin/HEAD)...(git rev-parse --abbrev-ref HEAD)
 end
+
+set GIT_DIR "$HOME/git"
+
+function repo
+  set repos (fd --type d --hidden ".git" "$GIT_DIR" | rg "$argv")
+  if test (count $repos) -eq 0
+    echo "No repository with pattern '$argv' found" >&2
+  else if test (count $repos) -eq 1
+    cd (dirname $repos[1])
+  else
+    echo "Found multiple repos..."
+    cd (dirname $repos | fzf)
+  end
+end
+
+alias gh='cd "$GIT_DIR"'
