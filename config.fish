e7cdeebadca5cc9ac8c12dc341d4cffa252c9aa2
Marcel Henrich <marcel@henrich-24.de>
2023-05-27 14:37:43 +0200

    Add keychain support

--- config.fish
+++ config.fish
@@ -14,3 +14,9 @@ abbr -a vim nvim
 function journal
   journalctl --since "$argv[1] 00:00" --until "$argv[1] 23:59" | grep -v crond | less
 end
+
+if type -f keychain 2>/dev/null
+  # for loading the SSH key
+  /usr/bin/keychain -q --nogui $HOME/.ssh/id_ed25519
+  source $HOME/.keychain/$HOST-sh
+end
